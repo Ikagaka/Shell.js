80c1606852da9410665b24678bc851c8c26cfb61
Narazaka <info@narazaka.net>
2016-07-14 23:04:39 +0900

    shell_data with PIXI

--- src/lib/shell_data.ts
+++ src/lib/shell_data.ts
@@ -1,14 +1,54 @@
+import {DataHandlerBase} from "./data_handler_base.ts";
 import {Descript} from "./descript.ts";
 
-export class ShellData {
-  static async buildFromDirectory(directory: any): Promise<ShellData> {
-      return;
+export class ShellData extends DataHandlerBase {
+  static async buildFromDirectory(directory: {[path: string]: Uint8Array}): Promise<ShellData> {
+    return Promise.all([
+      ShellData.getDescript(directory),
+      ShellData.getImages(directory),
+      ShellData.getSurfaces(directory),
+      ShellData.getSurfaceTable(directory),
+    ]).then((args) => {
+      return new ShellData(args[0], args[1], args[2], args[3]);
+    });
   }
-  
+
+  static selectSurfacePaths(directory: {[path: string]: any}) {
+    return Object.keys(directory).filter((path) => /(?:^|\b)surfaces.*\.txt$/i.test(path));
+  }
+
+  static selectSurfaces(directory: {[path: string]: Uint8Array}) {
+    return ShellData.selectSurfacePaths(directory).sort().map((path) => ShellData.bufferToString(directory[path]));
+  }
+
+  static parseSurfaces(str: string[]) {
+    return {}; // TODO
+  }
+
+  static getSurfaces(directory: {[path: string]: Uint8Array}) {
+    return ShellData.parseSurfaces(ShellData.selectSurfaces(directory));
+  }
+
+  static selectSurfaceTablePaths(directory: {[path: string]: any}) {
+    return Object.keys(directory).filter((path) => /(?:^|\b)surfacetable.*\.txt$/i.test(path));
+  }
+
+  static selectSurfaceTables(directory: {[path: string]: Uint8Array}) {
+    return ShellData.selectSurfacePaths(directory).sort().map((path) => ShellData.bufferToString(directory[path]));
+  }
+
+  static parseSurfaceTable(str: string[]) {
+    return {}; // TODO
+  }
+
+  static getSurfaceTable(directory: {[path: string]: Uint8Array}) {
+    return ShellData.parseSurfaceTable(ShellData.selectSurfaceTables(directory));
+  }
+
   descript: Descript;
 
-  constructor(surfaceResources: any, surfaceDefinitions: any) {
-
+  constructor(descript: Descript, surfaceResources: any, surfaceDefinition: any, surfaceTable: any) {
+    super();
   }
 
   unload() {
