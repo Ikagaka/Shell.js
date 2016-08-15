4c738c2c2a70423d0f7ca2e437a8fa3320aae7b1
legokichi <legokichi@gmail.com>
2016-08-15 18:21:24 +0900

    remove tslint tsfmt

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 const gulp = require('gulp');
-const tslint = require("tslint");
-const typescript = require('typescript')
+const typescript = require('typescript');
 const ts = require('gulp-typescript');
 const babel = require('gulp-babel');
 const espower = require('gulp-espower');
 const browserify = require('gulp-browserify');
-const gtslint = require("gulp-tslint");
-const gtsfmt = require('gulp-tsfmt');
-const runSequence = require('run-sequence');
+const runseq = require('run-sequence');
 
 const tsProject = ts.createProject('tsconfig.json', {typescript});
 
@@ -25,22 +22,11 @@ gulp.task('build:dist', ()=>{
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task("fmt", ()=>{
-    return tsProject.src()
-        .pipe(gtsfmt())
-        .pipe(gulp.dest('fmt'));
-});
-
-gulp.task("lint", ()=>{
-    tsProject.src()
-        .pipe(gtslint({tslint}))
-        .pipe(gtslint.report());
-});
-
 gulp.task('watch:src', ()=>{
     return gulp.watch('src/*.ts', ['build']);
 });
 
 gulp.task('default', ['build']);
-gulp.task('build', (cb)=> runSequence("build:lib", ["build:dist"], cb));
+gulp.task('build', (cb)=> runseq("build:lib", ["build:dist"], cb));
 gulp.task('watch', ['build', "watch:src"]);
+
