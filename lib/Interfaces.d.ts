511d877fefa5a9c57b9ea8440781d37ce2001d1a
legokichi <legokichi@gmail.com>
2016-08-26 20:56:10 +0900

    ready to merge

--- lib/Interfaces.d.ts
+++
@@ -1,8 +0,0 @@
-import { Descript } from '~ikagaka.shell.js/ShellConfig';
-import Surface from '~ikagaka.shell.js/Surface';
-export declare type BAL = {
-    descript: Descript;
-    canvas: HTMLCanvasElement;
-};
-export declare type Descript = Descript;
-export declare type Surface = Surface;
--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import _Balloon from "./Balloon";
-import _BalloonSurface from "./BalloonSurface";
-export declare var version: any;
-export declare var Balloon: typeof _Balloon;
-export declare var Blimp: typeof _BalloonSurface;
